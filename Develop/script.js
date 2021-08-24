4887dbbb392844d901b6abce0dfd86c333dc9fd6
Chris Tijerina <crtijerina1988@gmail.com>
2021-08-23 23:25:01 -0500

    created time blocks with approperte pm and am, next working on the local save via save button

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,33 @@
 $(document).ready(function () {
-var result = luxon.DateTime.local()
-$("#demo").text(result.toLocaleString({weekday: 'long', month: 'long', day: 'numeric' }));
+  var result = luxon.DateTime.local();
+  $("#demo").text(
+    result.toLocaleString({ weekday: "long", month: "long", day: "numeric" })
+  );
 
-// creating time blocks
-for(i= i < i> )
- 
+  // creating time blocks
+  for (let i = 9; i <= 17; i++) {
+    var row = document.createElement("div");
+    $(row).addClass("row");
+    $(".container").append(row);
 
+    var hourBlk = document.createElement("div");
+    $(hourBlk).addClass("hour col-1");
+    $(row).append(hourBlk);
 
+    if (i < 12) {
+      $(hourBlk).text(i + "am");
+    } else if ((i === 12)) {
+      $(hourBlk).text(i + "pm");
+    } else {
+      $(hourBlk).text((i % 12) + "pm");
+    }
 
+    var textArea = document.createElement("textarea");
+    $(textArea).addClass("time-block col-10 description");
+    $(row).append(textArea);
 
-
-
-
+    var DaButton = document.createElement("button");
+    $(DaButton).addClass("far fa-save saveBtn col-1");
+    $(row).append(DaButton);
+  }
 });
